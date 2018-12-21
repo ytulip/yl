a7fe09f82e34685c97f011e0781fd4226e4e59bd
zhuyan <yan.zhu01@bqjr.cn>
2018-12-21 10:23:38 +0800

    传参数！

--- resources/views/addresses.blade.php
+++ resources/views/addresses.blade.php
@@ -32,7 +32,7 @@
         @if(count($addressList))
         @foreach( $addressList as $address )
         <div class="address-item" style="margin-bottom: 20px;">
-            <div class="item-header" onclick="chooseAddress()">
+            <div class="item-header" onclick="chooseAddress({{$address->address_id}},'{{$address->address_name}}','{{$address->mobile}}','{{$address->pct_code_name}}','{{$address->address}}')">
                 <div><span>{{$address->address_name}}</span>&nbsp;&nbsp;<span>{{\App\Util\Kit::phoneHide($address->mobile)}}</span></div>
                 <p><span>{{$address->pct_code_name}}</span>&nbsp;&nbsp;<span>{{$address->address}}</span></p>
             </div>
@@ -62,8 +62,8 @@
     <script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"></script>
     <script>
         
-        function chooseAddress() {
-            wx.miniProgram.postMessage({ data: 'foo' });
+        function chooseAddress(id,name,phone,pct,address) {
+            wx.miniProgram.postMessage({ id: id, name:name,phone: phone,pct:pct,address:address});
             wx.miniProgram.navigateBack(
                 {
                     delta:1,
