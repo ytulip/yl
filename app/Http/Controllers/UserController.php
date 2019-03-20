2ab4878d7f879cdae9527a2ce39f77c92beb7520
zhuyan <yan.zhu01@bqjr.cn>
2019-03-20 13:31:29 +0800

    优惠券功能

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1248,7 +1248,7 @@ class UserController extends Controller
      */
     public function anyCouponList()
     {
-        $list = Coupon::where('user_id',Auth::id())->where('status',1)->where('expire_at','<=',date('Y-m-d'))->orderBy('id','desc')->get();
+        $list = Coupon::where('user_id',Auth::id())->where('status',1)->where('expire_at','>=',date('Y-m-d'))->orderBy('id','desc')->get();
         return $this->jsonReturn(1,$list);
     }
 }
