5111e755693927b06bbd7d17e161390613a9514e
childyi <yi@zhuyan.me>
2019-05-01 23:30:02 +0800

    代码提交

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -937,7 +937,7 @@ class UserController extends Controller
 
     public function anyUserOrder()
     {
-        $list = Order::where('user_id',Auth::id())->leftJoin('products','products.id','=','orders.product_id')->selectRaw('orders.*,products.type')->orderBy('orders.id','desc')->where('service_start_time','>=',date('Y-m-d'))->where('order_status','>',0)->where('type',1)->get();
+        $list = Order::where('user_id',Auth::id())->leftJoin('products','products.id','=','orders.product_id')->selectRaw('orders.*,products.type')->orderBy('orders.id','desc')->where('service_start_time','>=',date('Y-m-d'))->where('order_status','>',0)->get();
         if( !$list)
         {
             $list = [];
@@ -948,7 +948,7 @@ class UserController extends Controller
             $list[$key]->service_start_time_format = Kit::dateFormat4($item->service_start_time);
             if( $item->type != 1)
             {
-                $count = SubFoodOrders::where('order_id',$item->id)->where('date','>=',date('Y-m-d'))->count();
+                $count = SubFoodOrders::where('order_id',$item->id)->where('date','>=',date('Y-m-d'))->where('type',1)->count();
                 $list[$key]->days_count =  $count;
             }
         }
