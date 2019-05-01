2702751137430309fe4ccf77c21f6913a3a4fb7d
childyi <yi@zhuyan.me>
2019-05-01 23:28:38 +0800

    代码提交

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -937,7 +937,7 @@ class UserController extends Controller
 
     public function anyUserOrder()
     {
-        $list = Order::where('user_id',Auth::id())->leftJoin('products','products.id','=','orders.product_id')->selectRaw('orders.*,products.type')->orderBy('orders.id','desc')->where('service_start_time','>=',date('Y-m-d'))->where('order_status','>',0)->get();
+        $list = Order::where('user_id',Auth::id())->leftJoin('products','products.id','=','orders.product_id')->selectRaw('orders.*,products.type')->orderBy('orders.id','desc')->where('service_start_time','>=',date('Y-m-d'))->where('order_status','>',0)->where('type',1)->get();
         if( !$list)
         {
             $list = [];
