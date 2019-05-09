20e4b8d1ebcbc05f83df7422d9a0bf0e20fa5abe
zhuyan <yan.zhu01@bqjr.cn>
2019-05-09 14:06:45 +0800

    提交

--- resources/views/good_detail_food.blade.php
+++ resources/views/good_detail_food.blade.php
@@ -280,10 +280,10 @@
                 </div>
             </div>
 
-            <div class="fs-14-fc-7E7E7E-r pre-text show-hight" id='food_desc' style="margin-top: 14px;margin-bottom: 22px;line-height: 18px;overflow: hidden;">{{$product->food_desc}}</div>
+            <div class="fs-14-fc-7E7E7E-r pre-text show-hight" id='food_desc' style="margin-top: 14px;margin-bottom: 8px;line-height: 18px;overflow: hidden;">{{$product->food_desc}}</div>
 
             <div class="" style="cursor: pointer;margin-bottom: 24px;" onclick="showMore()" data-show id="show_line">
-                <img src="/images/icon_next_nor copy 2@3x.png" class="in-bl-v-m" width="16px;"/> <span class="in-bl v-a-m fs-18-fc-000000-r" style="font-size: 16px !important;">展开</span>
+                <img src="/images/icon_next_nor copy 2@3x.png" class="in-bl-v-m" width="13px;"/> <span class="in-bl v-a-m fs-18-fc-000000-r" style="font-size: 14px !important;">展开</span>
             </div>
 
 
