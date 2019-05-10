4f4595dd2c58d4f10896c156488965a8de933363
childyi <yi@zhuyan.me>
2019-05-10 21:58:30 +0800

    体检次数

--- app/Http/Controllers/FinaceController.php
+++ app/Http/Controllers/FinaceController.php
@@ -77,7 +77,7 @@ class FinaceController extends Controller
 
             }
             $user->expire_time= $expireMonth->format('Y-m-d');
-            $user->health_count = 4;
+            $user->health_count = 1;
 
 
             /**
